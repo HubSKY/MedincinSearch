62ba0745341ff11c66d8d9dc012bf427fde4a337
gongguiwei <1743292719@qq.com>
2013-12-10 11:13:38 +0800

    基本信息类，用于存放登录用户的信息（龚贵伟）

---
+++ basicInf.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedicineSearch
+{
+    public   class basicInf
+    {
+        static string Unoo=null ;
+        static string nickName =null;
+        public static string getUnoo()
+        {
+            return Unoo;
+        }
+        public static void setnickName( string s1)
+        {
+            nickName = s1;
+ 
+        }
+        public static string getnickName()
+        {
+            return nickName;
+        }
+        public static void setUnoo( string s1)
+        {
+            Unoo = s1;
+ 
+        }
+
+
+
+    }
+}
